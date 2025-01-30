db63e2e6f743b4d04f65f2895901112ae35b8103
serg-kas <serg_kas@mail.ru>
2025-01-30 20:12:23 +0300

    model folder

--- settings.py
+++ settings.py
@@ -121,7 +121,8 @@ ALLOWED_TYPES = ALLOWED_IMAGES + ['.pdf']
 
 # Параметры модели SAM2
 SAM2_config_file = "sam2_hiera_l.yaml"
-SAM2_checkpoint_file = "models/sam2_hiera_large.pt"
+# SAM2_checkpoint_file = "models/sam2_hiera_large.pt"
+SAM2_checkpoint_file = "models/sam2.1_hiera_large.pt"
 SAM2_force_cuda = True
 # SAM2_force_cuda = False
 SAM2_iou_threshold = 0.25
